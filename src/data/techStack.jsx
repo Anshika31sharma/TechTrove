d0e0f984836469b50e9d52425a1b6ca3d2c20a2e
Anshika31sharma <sharma.anshika7251@gmail.com>
2024-01-07 16:17:14 +0530

    added portfolio

---
+++ src/data/techStack.jsx
@@ -0,0 +1,45 @@
+import ReactJs, { JS, TailwindCSS,Bootstrap, GitHub, VSCode } from "../components/SkilIcons"
+
+
+const TechStack = [
+    {
+        id: 1,
+        icon: <ReactJs width={25} height={30} />,
+        name: "React",
+
+    },
+    {
+        id: 2,
+        icon: <JS width={22} height={30} />,
+        name: "Javascript",
+
+    },
+    {
+        id: 3,
+        icon: <TailwindCSS width={25} height={30} />,
+        name: "TailwindCSS",
+
+    },
+    {
+        id: 4,
+        icon: <Bootstrap width={25} height={30} />,
+        name: "Bootstrap",
+
+    },
+   
+    {
+        id: 5,
+        icon: <GitHub width={25} height={30} />,
+        name: "GitHub",
+
+    },
+    {
+        id: 6,
+        icon: <VSCode width={25} height={30} />,
+        name: "VsCode",
+
+    },
+]
+
+export default TechStack;
+
