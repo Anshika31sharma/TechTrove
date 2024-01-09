da92c4362e2cf9f1d906d4d33456fddd0443865d
Anshika31sharma <sharma.anshika7251@gmail.com>
2024-01-10 00:16:02 +0530

    added  Form Submission using EmailJS

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,28 @@
-import React, { useEffect, useState } from 'react'
-import SectionHeading from './SectionHeading'
-import { Person, GMail, Chat, Submit } from './Icons'
-import { validateForm } from '../utils/formValidation'
+import React, { useEffect, useState } from 'react';
+import SectionHeading from './SectionHeading';
+import { Person, GMail, Chat, Submit } from './Icons';
+import { validateForm } from '../utils/formValidation';
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+    const form = useRef();
+    
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_mhisziv', 'template_blfema9', form.current, 'XixhOWojGvXUyC-Hu')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
     const initialFormData = { name: "", email: "", message: "" };
-    const [formData, setFormData] = useState(initialFormData)
+    const [formData, setFormData] = useState(initialFormData);
     const initialErrData = { nameError: "", emailError: "", messageError: "" };
-    const [errData, setErrData] = useState(initialErrData)
+    const [errData, setErrData] = useState(initialErrData);
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -16,7 +31,7 @@ const Contact = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        validateForm(formData, setFormData, setErrData,initialFormData,initialErrData);
+        validateForm(formData, setFormData, setErrData, initialFormData, initialErrData);
     }
 
     useEffect(() => {
@@ -29,7 +44,7 @@ const Contact = () => {
             <div className='section'>
                 <SectionHeading heading="Contact" secondHeading="Fill the form to get in touch with me" />
                 <div className='flex justify-center items-center'>
-                    <form onSubmit={handleSubmit} className='flex gap-3 flex-col w-full md:w-1/2 '>
+                    <form ref={form} onSubmit={sendEmail} className='flex gap-3 flex-col w-full md:w-1/2 '>
                         <div className=" w-full">
                             <div className='relative'>
                                 <input type="text" name="name" placeholder="Enter Your Name" className={`peer form-input ${errData.nameError !== "" && "border-red-500"}`} value={formData.name} onChange={handleChange} />
@@ -46,15 +61,15 @@ const Contact = () => {
                         </div>
                         <div className=" w-full">
                             <div className="relative">
-                                <textarea name="message" placeholder='Your Meaasge' rows="10" cols="30"
-                                    className={`peer form-input ${errData.messageError !== "" && "border-red-500"}`} value={formData.message} onChange={handleChange} />
+                                <textarea name="message" placeholder='Your Message' rows="10" cols="30" className={`peer form-input ${errData.messageError !== "" && "border-red-500"}`} value={formData.message} onChange={handleChange} />
                                 <FormIcon name="chat" />
                             </div>
                             <ErrorBox message={errData.messageError} />
                         </div>
 
                         <button className='hover:text-blue-800 flex items-center justify-center mr-auto group text-white bg-gradient-to-b from-cyan-500 to-blue-500 rounded-md font-semibold px-12 py-2 hover:scale-[1.02] duration-500'>Let's Connect
-                            <span className="scale-0 -translate-x-10 origin-left group-hover:scale-100 group-hover:translate-x-3 duration-300 transition-all delay-200 ease-in-out"> <Submit />
+                            <span className="scale-0 -translate-x-10 origin-left group-hover:scale-100 group-hover:translate-x-3 duration-300 transition-all delay-200 ease-in-out">
+                                <Submit />
                             </span>
                         </button>
                     </form>
@@ -66,8 +81,6 @@ const Contact = () => {
 
 export default Contact;
 
-
-
 const FormIcon = ({ name }) => {
     return (
         <span className={`peer-placeholder-shown:grayscale peer-focus:grayscale-0 peer-active:grayscale-0 absolute left-3 ${name === "chat" ? "top-[0.8rem]" : "top-1/2 -translate-y-1/2"}`}>
@@ -77,6 +90,7 @@ const FormIcon = ({ name }) => {
         </span>
     )
 }
+
 const ErrorBox = ({ message }) => {
     return (
         <div className=' text-sm min-h-[1.3rem] text-red-500 px-3'>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import { Link } from 'react-scroll'
-import navLinks from '../data/navlinks'
+import React from 'react';
+import { Link } from 'react-scroll';
+import navLinks from '../data/navlinks';
+
 const Navigation = ({ ulClass, liClass, handleClick }) => {
     return (
         <ul className={ulClass}>
-            {navLinks.map(({ id, link, href }) => {
+            {navLinks.map(({ id, link }) => {
                 return (
-                    <li key={id}
-                        className={`mx-4 py-2 capitalize font-medium  cursor-pointer text-gray-500 hover:scale-[1.15] border-b-2 border-transparent hover:border-b-gray-300 duration-500 hover:text-gray-300 ${liClass}`}>
-                        {id === 6 ?
-                            <a href={href} target="_blank" rel='noferrer'>{link}</a>
-                            :
-                            <Link
-                                onClick={handleClick}
-                                to={link} smooth duration={500}> {link}
+                    <li
+                        key={id}
+                        className={`mx-4 py-2 capitalize font-medium  cursor-pointer text-gray-500 hover:scale-[1.15] border-b-2 border-transparent hover:border-b-gray-300 duration-500 hover:text-gray-300 ${liClass}`}
+                    >
+                        {id === 2? (
+                            <a href={`#${link}`} target="_blank" rel='noferrer'>
+                                {link}
+                            </a>
+                        ) : (
+                            <Link onClick={handleClick} to={link} smooth duration={500}>
+                                {link}
                             </Link>
-                        }
+                        )}
                     </li>
-                )
+                );
             })}
         </ul>
-    )
-}
+    );
+};
 
-export default Navigation
+export default Navigation;
